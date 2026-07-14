da4ce40f096a5b74512cb42782c0ed79a00d5790
cardioailive1 <tonywell@cardioailive.com>
2026-07-14 13:23:25 -0400

    Update securityHeaders.js

--- src/api/middleware/securityHeaders.js
+++ src/api/middleware/securityHeaders.js
@@ -2,14 +2,9 @@
 
 const { v4: uuidv4 } = require('uuid');
 
-// Additional security headers beyond helmet
 function securityHeaders(req, res, next) {
-  res.setHeader('X-Request-ID',          req.headers['x-request-id'] || uuidv4());
-  res.setHeader('X-Content-Type-Options','nosniff');
-  res.setHeader('X-Frame-Options',       'DENY');
-  res.setHeader('Referrer-Policy',       'strict-origin-when-cross-origin');
-  res.setHeader('Permissions-Policy',    'camera=(), microphone=(), geolocation=()');
-  res.setHeader('X-Powered-By',          'NexGen Ultra');
+  res.setHeader('X-Request-ID', req.headers['x-request-id'] || uuidv4());
+  res.setHeader('X-Powered-By', 'NexGen Ultra');
   next();
 }
 
